ca9ae55a18b98dfa5e18bc72e8b38780cfed9b20
sdannyandres <sdannyandres@gmail.com>
2022-11-20 18:51:29 +0100

    params

--- app.js
+++ app.js
@@ -12,17 +12,38 @@ app.get("/", function (req, res) {
 
 
 app.post("/echoPost", (req, res) => {
-    res.send({dody:req.body})
+    res.send({body:req.body})
 
 })
 app.post("/echoPostJson", (req, res) => {
-    res.send({dodyJson:req.body})
+    res.send({bodyJson:req.body})
 
 })
 app.post("/echoPostExtended", (req, res) => {
-    res.send({dodyJson:req.body})
+    res.send({bodyJson:req.body})
 
 })
+app.post("/echoPostJson", (req, res) =>{
+    res.send({bodyJson:req.body, qs:req.query})
+})
+
+app.post("/echoParamsPost/:cliente/facturas/:factura", (req, res) =>{
+    res.send({
+        body:req.body, 
+        query:req.query,
+        params:req.params
+    })
+})
+
+app.get("/echoParamsGet/:cliente/facturas/:factura", (req, res) =>{
+    res.send({
+        body:req.body, 
+        query:req.query,
+        params:req.params
+    })
+})
+
+
 app.post("/addUser", (req, res) => {
     res.status(200).send("He añadido un usuario")
     })
