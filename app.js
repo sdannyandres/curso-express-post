d2f9c7b0b06c04ed446335b1b2f328675d42687e
sdannyandres <sdannyandres@gmail.com>
2022-11-22 00:26:00 +0100

    construccion base de datos

--- app.js
+++ app.js
@@ -3,28 +3,35 @@ const morgan = require("morgan");
 const fs = require("fs")
 const fileUpload = require("express-fileupload");
 const app = ex();
+const { Pool } = require('pg');
 
+const pool = new Pool({
+    localhost: "localhost",
+    port: 5499,
+    user: "postgres",
+    password: "123456"
+})
 
-const logOutputErrores = fs.createWriteStream("uploads/logsErrores.txt",{
+const logOutputErrores = fs.createWriteStream("uploads/logsErrores.txt", {
     flags: "a"
 })
-const logOutputBuenos = fs.createWriteStream("uploads/logsBuenos.txt",{
+const logOutputBuenos = fs.createWriteStream("uploads/logsBuenos.txt", {
     flags: "a"
 })
 app.use(morgan('combined', {
-    skip:(req, res) => res.statusCode < 400,
-    stream:logOutputErrores 
+    skip: (req, res) => res.statusCode < 400,
+    stream: logOutputErrores
 }))
 
 app.use(morgan('tiny', {
-    skip:(req, res) => res.statusCode < 400,
-    stream:logOutputErrores 
+    skip: (req, res) => res.statusCode < 400,
+    stream: logOutputErrores
 }))
 
 
 
-app.use(ex.static("public",{
-    index:"myIndex.html"
+app.use(ex.static("public", {
+    index: "myIndex.html"
 }))
 
 app.use(fileUpload({
@@ -32,30 +39,44 @@ app.use(fileUpload({
 }))
 
 app.use(ex.json())
-app.use(ex.urlencoded({extended:true}))
+app.use(ex.urlencoded({ extended: true }))
 
-app.get("/welcome",function(req,res){
+app.get("/welcome", function (req, res) {
     res.send("Welcome to my server")
 })
 
 
 
-app.post("/uploadFicheros",async(req,res)=>{
-     const f1 = req.files.file1
-     await f1.mv('uploads/${f1.name}')
-     res.send({body:req.body, fichero:{
-        nombre : req.files.file1.name
-     }})
+app.post("/uploadFicheros", async (req, res) => {
+    const f1 = req.files.file1
+    await f1.mv('uploads/${f1.name}')
+    res.send({
+        body: req.body, fichero: {
+            nombre: req.files.file1.name
+        }
+    })
 
 })
 
 app.post("/uploadFicherosMultiple", async (req, res) => {
-    for (const [index,file] of req.files.ficheros.entries()) {
+    for (const [index, file] of req.files.ficheros.entries()) {
         await file.mv('uploads/${file.name}')
     }
     res.send("ficheros subidos")
 }
 )
-    
-        
+
+app.get("/bdd", async (req, res) => {
+    try {
+        const respuesta = await pool.query("select now()fecha");
+        res.send(respuesta.rows)
+    } catch (error) {
+        res.status(500).send({ error })
+
+    }
+
+})
+
+
+
 app.listen(3344)
